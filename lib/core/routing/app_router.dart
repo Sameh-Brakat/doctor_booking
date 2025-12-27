dd4ab47dbdba629be87ec6102914dfadfa8f59e0
Sameh Barakat <sameh13652@gmail.com>
2025-12-27 21:16:23 +0200

    feat: Add initial application setup including onboarding and login screens, routing, theming, and the main application widget.

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,23 @@
+import 'package:doctor_booking/core/routing/routes.dart';
+import 'package:doctor_booking/features/login/ui/login_screen.dart';
+import 'package:doctor_booking/features/onboarding/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(
+          builder: (context) => const OnBoardingScreen(),
+        );
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (context) => const LoginScreen());
+      default:
+        return MaterialPageRoute(
+          builder: (context) => Scaffold(
+            body: Center(child: Text('No route found for ${settings.name}')),
+          ),
+        );
+    }
+  }
+}
